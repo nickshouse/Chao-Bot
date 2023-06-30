9bd74dc52da96967ff641431ca5d6a8f3ffb7299
nickshouse <nickshouse@proton.me>
2023-06-30 23:05:43 +0000

    Jun 30, 2023, 6:05 PM

--- src/cogs/chao_stats.py
+++ src/cogs/chao_stats.py
@@ -1,33 +1,37 @@
 from discord.ext import commands
+import random
 
 class ChaoStats(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.database_cog = self.bot.get_cog("Database")
 
-    async def get_stats(self, guild_id, user_id):
-        stats = ['fly', 'run', 'swim', 'power', 'stamina']
-        user_stats = {stat: await self.database_cog.get_data(guild_id, user_id, stat) for stat in stats}
-        return user_stats
+    async def get_stat(self, guild_id, user_id, stat):
+        stat_data = await self.database_cog.get_data(guild_id, user_id, f"{stat}_level")
+        tick_data = await self.database_cog.get_data(guild_id, user_id, f"{stat}_ticks")
+        return stat_data, tick_data
 
-    async def set_stats(self, guild_id, user_id, stats):
-        for stat, value in stats.items():
-            self.database_cog.data_queue.append((guild_id, user_id, value, stat))
+    async def set_stat(self, guild_id, user_id, stat, stat_value, tick_value):
+        self.database_cog.data_queue.append((guild_id, user_id, stat_value, f"{stat}_level"))
+        self.database_cog.data_queue.append((guild_id, user_id, tick_value, f"{stat}_ticks"))
 
-    @commands.command()
-    async def show_stats(self, ctx):
+    async def feed_chao(self, ctx, stat):
         guild_id = ctx.guild.id
         user_id = ctx.author.id
-        user_stats = await self.get_stats(guild_id, user_id)
-        stats_message = '\n'.join(f"{stat.capitalize()}: {value}" for stat, value in user_stats.items())
-        await ctx.send(f"Your Chao's stats:\n{stats_message}")
+        stat_data, tick_data = await self.get_stat(guild_id, user_id, stat)
+
+        tick_increase = random.randint(2, 3)
+        new_tick_data = (tick_data or 0) + tick_increase
+        level_increase = new_tick_data // 10
+        new_stat_data = (stat_data or 0) + level_increase
+        new_tick_data %= 10
+
+        await self.set_stat(guild_id, user_id, stat, new_stat_data, new_tick_data)
+        await ctx.send(f"Your Chao's {stat} level is now {new_stat_data} and it has {new_tick_data} ticks towards the next level.")
 
     @commands.command()
-    async def set_stat(self, ctx, stat, value: int):
-        guild_id = ctx.guild.id
-        user_id = ctx.author.id
-        await self.set_stats(guild_id, user_id, {stat: value})
-        await ctx.send(f"Set {stat} to {value}.")
+    async def feed(self, ctx, stat):
+        await self.feed_chao(ctx, stat)
 
 async def setup(bot):
     await bot.add_cog(ChaoStats(bot))
