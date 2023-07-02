bf70eaa85ccd662f6e5318e1ea8c8fc31f87c1eb
nickshouse <nickshouse@proton.me>
2023-07-02 21:15:18 +0000

    Jul 2, 2023, 4:15 PM

--- src/cogs/database.py
+++ src/cogs/database.py
@@ -7,10 +7,24 @@ from datetime import datetime
 
 class Database(commands.Cog):
     def __init__(self, bot):
-            self.bot = bot
-            self.data_path = '../database'  
-            os.makedirs(self.data_path, exist_ok=True)
- 
+        self.bot = bot
+        self.data_path = '../database'  
+        os.makedirs(self.data_path, exist_ok=True)
+
+    async def get_inventory(self, guild_id, user_id):
+        filename = f"{self.data_path}/{guild_id}_{user_id}.parquet"
+        if os.path.exists(filename):
+            df = pd.read_parquet(filename)
+            return df
+        return None
+
+    async def store_inventory(self, guild_id, user_id, inventory):
+        filename = f"{self.data_path}/{guild_id}_{user_id}.parquet"
+        df = pd.DataFrame(inventory, columns=['time', 'type', 'value'])
+        df.to_parquet(filename)
+        
+    # get_data and store_data methods
+
  
 async def setup(bot):
     await bot.add_cog(Database(bot))
