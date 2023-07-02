dfeecfba09dd7b035c0cf047848cd440795d28cb
nickshouse <nickshouse@proton.me>
2023-07-02 21:17:40 +0000

    Jul 2, 2023, 4:17 PM

--- src/cogs/database.py
+++ src/cogs/database.py
@@ -12,19 +12,19 @@ class Database(commands.Cog):
         os.makedirs(self.data_path, exist_ok=True)
 
     async def get_inventory(self, guild_id, user_id):
-        filename = f"{self.data_path}/{guild_id}_{user_id}.parquet"
+        dir_path = f"{self.data_path}/{guild_id}/{user_id}/user_data"
+        filename = f"{dir_path}/inventory.parquet"
         if os.path.exists(filename):
             df = pd.read_parquet(filename)
             return df
         return None
 
     async def store_inventory(self, guild_id, user_id, inventory):
-        filename = f"{self.data_path}/{guild_id}_{user_id}.parquet"
+        dir_path = f"{self.data_path}/{guild_id}/{user_id}/user_data"
+        os.makedirs(dir_path, exist_ok=True)
+        filename = f"{dir_path}/inventory.parquet"
         df = pd.DataFrame(inventory, columns=['time', 'type', 'value'])
         df.to_parquet(filename)
-        
-    # get_data and store_data methods
-
  
 async def setup(bot):
     await bot.add_cog(Database(bot))
