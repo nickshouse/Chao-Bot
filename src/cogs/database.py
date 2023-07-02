e822238055d8e1226ee3ab59fa95f4f0de70a4a8
nickshouse <nickshouse@proton.me>
2023-07-02 22:43:42 +0000

    Jul 2, 2023, 5:43 PM

--- src/cogs/database.py
+++ src/cogs/database.py
@@ -11,6 +11,21 @@ class Database(commands.Cog):
         self.data_path = '../database'  
         os.makedirs(self.data_path, exist_ok=True)
 
+    async def get_rings(self, guild_id, user_id):
+        dir_path = f"{self.data_path}/{guild_id}/{user_id}/user_data"
+        filename = f"{dir_path}/rings.parquet"
+        if os.path.exists(filename):
+            df = pd.read_parquet(filename)
+            return df
+        return None
+
+    async def store_rings(self, guild_id, user_id, value):
+        dir_path = f"{self.data_path}/{guild_id}/{user_id}/user_data"
+        os.makedirs(dir_path, exist_ok=True)
+        filename = f"{dir_path}/rings.parquet"
+        df = pd.DataFrame([{'time': datetime.now(), 'value': value}])
+        df.to_parquet(filename)
+
     async def get_inventory(self, guild_id, user_id):
         dir_path = f"{self.data_path}/{guild_id}/{user_id}/user_data"
         filename = f"{dir_path}/inventory.parquet"
