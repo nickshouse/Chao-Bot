0222d724388c1ba8bd7839f03aa8b9938f906338
nickshouse <nickshouse@proton.me>
2023-07-05 15:06:51 +0000

    Jul 5, 2023, 10:06 AM

--- src/cogs/chao.py
+++ src/cogs/chao.py
@@ -1,3 +1,5 @@
+import asyncio
+import random
 from discord.ext import commands
 
 class Chao(commands.Cog):
@@ -6,6 +8,29 @@ class Chao(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+
+
+    @commands.command()
+    async def give_egg(self, ctx):
+        """Give a Chao Egg to the user and hatch it after 5 seconds"""
+        color = random.choice(self.chao_colors)
+        egg_type = random.choice(self.chao_types)
+        egg = f"{color} {egg_type} Chao Egg"
+
+        await ctx.send(f"You received a {egg}! It will hatch in 5 seconds.")
+        await asyncio.sleep(5)
+        await self.hatch_egg(ctx, egg)
+
+    async def hatch_egg(self, ctx, egg):
+        """Hatch the user's Chao Egg"""
+        guild_id = ctx.guild.id
+        user_id = ctx.author.id
+        color, _, egg_type = egg.split(' ', 2)
+        chao = {"color": color, "type": egg_type.rstrip(" Chao Egg")}
+
+        await self.bot.get_cog("Database").store_chao(guild_id, user_id, chao)
+        await ctx.send(f"Your {egg} has hatched into a {color} {egg_type} Chao!")
+
 
 async def setup(bot):
     await bot.add_cog(Chao(bot))
--- src/cogs/database.py
+++ src/cogs/database.py
@@ -10,6 +10,14 @@ class Database(commands.Cog):
         self.data_path = '../database'  
         os.makedirs(self.data_path, exist_ok=True)
 
+
+    async def store_rings(self, guild_id, user_id, value):
+        dir_path = f"{self.data_path}/{guild_id}/{user_id}/user_data"
+        os.makedirs(dir_path, exist_ok=True)
+        filename = f"{dir_path}/rings.parquet"
+        df = pd.DataFrame([{'value': value}])
+        df.to_parquet(filename)
+
     async def get_rings(self, guild_id, user_id):
         dir_path = f"{self.data_path}/{guild_id}/{user_id}/user_data"
         filename = f"{dir_path}/rings.parquet"
@@ -19,11 +27,11 @@ class Database(commands.Cog):
         return 0  # return 0 if the file doesn't exist
 
 
-    async def store_rings(self, guild_id, user_id, value):
+    async def store_inventory(self, guild_id, user_id, inventory):
         dir_path = f"{self.data_path}/{guild_id}/{user_id}/user_data"
         os.makedirs(dir_path, exist_ok=True)
-        filename = f"{dir_path}/rings.parquet"
-        df = pd.DataFrame([{'value': value}])
+        filename = f"{dir_path}/inventory.parquet"
+        df = pd.DataFrame(inventory, columns=['quantity', 'item'])
         df.to_parquet(filename)
 
     async def get_inventory(self, guild_id, user_id):
@@ -34,13 +42,24 @@ class Database(commands.Cog):
             return df
         return None
 
-    async def store_inventory(self, guild_id, user_id, inventory):
-        dir_path = f"{self.data_path}/{guild_id}/{user_id}/user_data"
+
+    async def store_chao(self, guild_id, user_id, chao):
+        dir_path = f"{self.data_path}/chao_data/{guild_id}/{user_id}"
         os.makedirs(dir_path, exist_ok=True)
-        filename = f"{dir_path}/inventory.parquet"
-        df = pd.DataFrame(inventory, columns=['quantity', 'item'])
+        filename = f"{dir_path}/chao_{len(os.listdir(dir_path)) + 1}.parquet"
+        df = pd.DataFrame(chao, index=[0])
         df.to_parquet(filename)
+
+    async def get_chao(self, guild_id, user_id):
+        dir_path = f"{self.data_path}/chao_data/{guild_id}/{user_id}"
+        chao = []
+        for file in os.listdir(dir_path):
+            if file.endswith(".parquet"):
+                df = pd.read_parquet(f"{dir_path}/{file}")
+                chao.append(df.to_dict(orient='records')[0])
+        return chao
         
+
 async def setup(bot):
     await bot.add_cog(Database(bot))
     print("Database cog loaded")
