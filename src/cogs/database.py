2f830f5e2238e03ce1e3f709f9317de3f951503e
nickshouse <nickshouse@proton.me>
2023-07-05 21:33:22 +0000

    Jul 5, 2023, 4:33 PM

--- src/cogs/database.py
+++ src/cogs/database.py
@@ -50,7 +50,69 @@ class Database(commands.Cog):
             await loop.run_in_executor(self.executor, data.to_parquet, filename)
             self.cache.put(filename, data)
 
-    # ...rest of your methods remain the same...
+    async def store_rings(self, guild_id, user_id, value):
+        dir_path = f"{self.data_path}/{guild_id}/{user_id}/user_data"
+        os.makedirs(dir_path, exist_ok=True)
+        filename = f"{dir_path}/rings.parquet"
+        df = pd.DataFrame([{'value': value}])
+        await self.write_file(filename, df)
+
+    async def get_rings(self, guild_id, user_id):
+        dir_path = f"{self.data_path}/{guild_id}/{user_id}/user_data"
+        filename = f"{dir_path}/rings.parquet"
+        df = await self.get_file(filename)
+        return df['value'].sum() if df is not None else 0
+
+    async def store_inventory(self, guild_id, user_id, inventory):
+        dir_path = f"{self.data_path}/{guild_id}/{user_id}/user_data"
+        os.makedirs(dir_path, exist_ok=True)
+        filename = f"{dir_path}/inventory.parquet"
+        df = pd.DataFrame(inventory, columns=['quantity', 'item'])
+        await self.write_file(filename, df)
+
+    async def get_inventory(self, guild_id, user_id):
+        dir_path = f"{self.data_path}/{guild_id}/{user_id}/user_data"
+        filename = f"{dir_path}/inventory.parquet"
+        return await self.get_file(filename)
+
+    async def store_chao(self, guild_id, user_id, chao):
+        dir_path = f"{self.data_path}/{guild_id}/{user_id}/chao_data"
+        os.makedirs(dir_path, exist_ok=True)
+        filename = f"{dir_path}/{chao['name']}.parquet"
+        df = await self.get_file(filename)
+        if df is not None:  # If the chao already exists, we need to update it
+            df.update(pd.DataFrame(chao, index=[0]))
+        else:  # If it's a new chao, we create it
+            df = pd.DataFrame(chao, index=[0])
+        await self.write_file(filename, df)
+
+    async def get_chao(self, guild_id, user_id):
+        dir_path = f"{self.data_path}/{guild_id}/{user_id}/chao_data"
+        chao = []
+        for file in os.listdir(dir_path):
+            if file.endswith(".parquet"):
+                df = await self.get_file(f"{dir_path}/{file}")
+                chao_dict = df.to_dict(orient='records')[0]
+                if 'name' in chao_dict:
+                    chao.append(chao_dict)
+        return chao
+
+    async def store_nickname(self, guild_id, user_id, nickname):
+        dir_path = f"{self.data_path}/{guild_id}/{user_id}/user_data"
+        os.makedirs(dir_path, exist_ok=True)
+        filename = f"{dir_path}/nicknames.parquet"
+        df = await self.get_file(filename)
+        new_data = pd.DataFrame([{'time': datetime.datetime.now(), 'nickname': nickname}])
+        if df is not None:
+            df = df.append(new_data, ignore_index=True)
+        else:
+            df = new_data
+        await self.write_file(filename, df)
+
+    async def get_nickname(self, guild_id, user_id):
+        dir_path = f"{self.data_path}/{guild_id}/{user_id}/user_data"
+        filename = f"{dir_path}/nicknames.parquet"
+        return await self.get_file(filename)
 
 async def setup(bot):
     await bot.add_cog(Database(bot))
