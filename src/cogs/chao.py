ccf437c5acb8e5d18f42a1a4c7dcb15cd7a17789
nickshouse <nickshouse@proton.me>
2023-07-06 03:37:03 +0000

    Jul 5, 2023, 10:37 PM

--- src/cogs/chao.py
+++ src/cogs/chao.py
@@ -1,6 +1,7 @@
 import asyncio
 import random
 import datetime
+import discord
 from discord.ext import commands
 
 
@@ -17,24 +18,29 @@ class Chao(commands.Cog):
         color = random.choice(self.chao_colors)
         chao_type = random.choice(self.chao_types)
         chao_name = self.bot.cogs['FortuneTeller'].generate_chao_name()
+
+        stats = ['fly', 'run', 'swim', 'power', 'stamina', 'luck', 'intelligence']
+        chao_stats = {stat: {
+            'grade': 'F',
+            'ticks': 0,
+            'exp': 0,
+            'level': 0,
+            'value': random.randint(1, 100)
+        } for stat in stats}
+
         chao = {
             'name': chao_name,
             'color': color,
             'type': chao_type,
             'hatched': 0,
             'birth_date': None,
-            'fly': random.randint(1, 100),
-            'run': random.randint(1, 100),
-            'swim': random.randint(1, 100),
-            'power': random.randint(1, 100),
-            'stamina': random.randint(1, 100),
-            'luck': random.randint(1, 100),
-            'intelligence': random.randint(1, 100),
             'hunger': 100,
             'HP': 100,
             'age': 0,
-            'alignment': 'Neutral'
+            'alignment': 'Neutral',
+            **chao_stats
         }
+        
         await self.bot.cogs['Database'].store_chao(ctx.guild.id, ctx.author.id, chao)
         await ctx.send(f"You received a {color} {chao_type} Chao Egg named {chao_name}! It will hatch in 5 seconds.")
         await asyncio.sleep(5)
@@ -42,6 +48,27 @@ class Chao(commands.Cog):
         chao['birth_date'] = datetime.datetime.now().date()
         await self.bot.cogs['Database'].store_chao(ctx.guild.id, ctx.author.id, chao)
         await ctx.send(f"Your {chao_name} Egg has hatched into a {color} {chao_type} Chao named {chao_name}!")
+
+
+    @commands.command()
+    async def stats(self, ctx, chao_name):
+        """Displays the stats of a specific Chao"""
+        chao_data = await self.bot.cogs['Database'].get_chao(ctx.guild.id, ctx.author.id)
+        
+        for chao in chao_data:
+            if chao['name'].lower() == chao_name.lower():
+                embed = discord.Embed(title=f"{chao['name']}'s Stats", color=0x00ff00)
+                stats = ['fly', 'run', 'swim', 'power', 'stamina', 'luck', 'intelligence']
+                for stat in stats:
+                    embed.add_field(name=stat.capitalize(), value=f"Grade: {chao[stat]['grade']}\nTicks: {chao[stat]['ticks']}\nExp: {chao[stat]['exp']}\nLevel: {chao[stat]['level']}\nValue: {chao[stat]['value']}", inline=True)
+                embed.add_field(name='Age', value=chao['age'], inline=False)
+                embed.add_field(name='HP', value=chao['HP'], inline=False)
+                embed.add_field(name='Hunger', value=chao['hunger'], inline=False)
+                embed.add_field(name='Alignment', value=chao['alignment'], inline=False)
+                await ctx.send(embed=embed)
+                return
+
+        await ctx.send(f"No Chao named {chao_name} was found.")
 
 async def setup(bot):
     await bot.add_cog(Chao(bot))
