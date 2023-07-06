f57d2b26ee488829784591dc05e3e4a2df4be47b
nickshouse <nickshouse@proton.me>
2023-07-06 03:26:59 +0000

    Jul 5, 2023, 10:26 PM

--- src/cogs/chao.py
+++ src/cogs/chao.py
@@ -11,28 +11,37 @@ class Chao(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def give_egg(self, ctx):
-        """Give a Chao Egg to the user"""
-        color = random.choice(self.chao_colors)
-        chao_type = random.choice(self.chao_types)
-        chao_name = self.bot.cogs['FortuneTeller'].generate_chao_name()
-        chao = {
-            'name': chao_name,
-            'color': color,
-            'type': chao_type,
-            'hatched': 0,
-            'birth_date': None
-        }
-        await self.bot.cogs['Database'].store_chao(ctx.guild.id, ctx.author.id, chao)
-        await ctx.send(f"You received a {color} {chao_type} Chao Egg named {chao_name}! It will hatch in 5 seconds.")
-        await asyncio.sleep(5)
-        chao['hatched'] = 1
-        chao['birth_date'] = datetime.datetime.now().date()
-        await self.bot.cogs['Database'].store_chao(ctx.guild.id, ctx.author.id, chao)
-        await ctx.send(f"Your {chao_name} Egg has hatched into a {color} {chao_type} Chao named {chao_name}!")
-
-
+@commands.command()
+async def give_egg(self, ctx):
+    """Give a Chao Egg to the user"""
+    color = random.choice(self.chao_colors)
+    chao_type = random.choice(self.chao_types)
+    chao_name = self.bot.cogs['FortuneTeller'].generate_chao_name()
+    chao = {
+        'name': chao_name,
+        'color': color,
+        'type': chao_type,
+        'hatched': 0,
+        'birth_date': None,
+        'fly': random.randint(1, 100),
+        'run': random.randint(1, 100),
+        'swim': random.randint(1, 100),
+        'power': random.randint(1, 100),
+        'stamina': random.randint(1, 100),
+        'luck': random.randint(1, 100),
+        'intelligence': random.randint(1, 100),
+        'hunger': 100,
+        'HP': 100,
+        'age': 0,
+        'alignment': 'Neutral'
+    }
+    await self.bot.cogs['Database'].store_chao(ctx.guild.id, ctx.author.id, chao)
+    await ctx.send(f"You received a {color} {chao_type} Chao Egg named {chao_name}! It will hatch in 5 seconds.")
+    await asyncio.sleep(5)
+    chao['hatched'] = 1
+    chao['birth_date'] = datetime.datetime.now().date()
+    await self.bot.cogs['Database'].store_chao(ctx.guild.id, ctx.author.id, chao)
+    await ctx.send(f"Your {chao_name} Egg has hatched into a {color} {chao_type} Chao named {chao_name}!")
 
 async def setup(bot):
     await bot.add_cog(Chao(bot))
