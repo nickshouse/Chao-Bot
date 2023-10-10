70f1f848b48f39f82b735c316023aa999dc56c9f
nickshouse <nickshouse@proton.me>
2023-10-10 14:33:33 -0500

    Refactor `paste_image` method to accept tick positions and the number of ticks for power, swim, stamina, fly, run, and hp.

--- src/cogs/generator.py
+++ src/cogs/generator.py
@@ -7,7 +7,7 @@ class Generator(commands.Cog):
         self.bot = bot
 
     @staticmethod
-    def paste_image(template_path, overlay_path, output_path, start_position, ticks):
+    def paste_image(template_path, overlay_path, output_path, tick_positions, power_ticks, swim_ticks, stamina_ticks, fly_ticks, run_ticks, hp_ticks):
         template = Image.open(template_path)
         overlay = Image.open(overlay_path)
 
@@ -15,35 +15,45 @@ class Generator(commands.Cog):
         if overlay.mode != 'RGBA':
             overlay = overlay.convert('RGBA')
 
-        # Determine the position for each tick and paste the overlay image
-        for i in range(ticks):
-            x_position = start_position[0] + i * 118  # 118 pixels apart on the x-axis
-            position = (x_position, start_position[1])  # Same y-axis
-            template.paste(overlay, position, overlay)
+        # Function to paste ticks at a specified start position
+        def paste_ticks(start_position, ticks):
+            for i in range(ticks):
+                x_position = start_position[0] + i * 118  # 118 pixels apart on the x-axis
+                position = (x_position, start_position[1])  # Same y-axis
+                template.paste(overlay, position, overlay)
+
+        # Paste power, swim, stamina, fly, run, and hp ticks
+        for position, ticks in zip(tick_positions, [power_ticks, swim_ticks, stamina_ticks, fly_ticks, run_ticks, hp_ticks]):
+            paste_ticks(position, ticks)
 
         template.save(output_path)
 
     @commands.command()
-    async def generate_image(self, ctx, chao_name, power_ticks=None):
-        # If power_ticks is None, fetch it from the database as before.
-        if power_ticks is None:
+    async def generate_image(self, ctx, chao_name, power_ticks=None, swim_ticks=None, stamina_ticks=None, fly_ticks=None, run_ticks=None, hp_ticks=None):
+        # If ticks are None, fetch them from the database
+        if any(tick is None for tick in [power_ticks, swim_ticks, stamina_ticks, fly_ticks, run_ticks, hp_ticks]):
             chao_list = await self.bot.cogs['Database'].get_chao(ctx.guild.id, ctx.author.id)
             chao_to_view = next((chao for chao in chao_list if chao['name'] == chao_name), None)
             if chao_to_view is None:
                 await ctx.send(f"You don't have a Chao named {chao_name}.")
                 return
-            power_ticks = chao_to_view['power_ticks']  # Get the power ticks from the database
-        
+            power_ticks = chao_to_view['power_ticks']
+            swim_ticks = chao_to_view['swim_ticks']
+            stamina_ticks = chao_to_view['stamina_ticks']
+            fly_ticks = chao_to_view['fly_ticks']
+            run_ticks = chao_to_view['run_ticks']
+            hp_ticks = chao_to_view['hp_ticks']  # Get the hp ticks from the database
+
         # Paths to the images
         template_path = '../assets/stats_template.png'
         overlay_path = '../assets/tick_filled.png'
         output_path = './output_image.png'
 
-        # Starting position where the first overlay image should be pasted onto the template
-        start_position = (178, 1162)
+        # Starting positions for power, swim, stamina, fly, run, and hp ticks
+        tick_positions = [(178, 1162), (178, 351), (178, 1448), (178, 625), (178, 890), (178, 1729)]
 
         # Call the function
-        self.paste_image(template_path, overlay_path, output_path, start_position, power_ticks)
+        self.paste_image(template_path, overlay_path, output_path, tick_positions, power_ticks, swim_ticks, stamina_ticks, fly_ticks, run_ticks, hp_ticks)
 
         # Send the generated image to the channel
         with open(output_path, 'rb') as file:
