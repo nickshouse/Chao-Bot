a827d3ac3b33eba19ba6a2fec2e935ecbccc311b
nickshouse <nickshouse@proton.me>
2023-10-25 23:12:00 -0500

    Refactor `Chao` class in `chao.py` to improve code readability and maintainability. Improve variable naming and remove unused imports.

--- src/cogs/chao.py
+++ src/cogs/chao.py
@@ -1,36 +1,32 @@
 import asyncio
 import random
 import datetime
-import discord
 from discord.ext import commands
-from typing import Dict, List, Optional
-import logging
-
-log = logging.getLogger(__name__)
+from typing import Dict, Optional, List
 
 class Chao(commands.Cog):
-    chao_colors = ['White', 'Blue', 'Red', 'Yellow', 'Orange', 'Sky Blue', 'Pink', 'Green', 'Mint', 'Brown', 'Purple', 'Grey', 'Lime Green', 'Black']
-    chao_types = ['Monotone', 'Two-tone', 'Jewel Monotone', 'Shiny Monotone', 'Jewel Two-tone', 'Shiny Two-tone']
-    grade_to_value = {'F': -1, 'E': 0, 'D': 1, 'C': 2, 'B': 3, 'A': 4, 'S': 5, 'X': 6}  # Mapping from grade to value
+    CHAO_COLORS = ['White', 'Blue', 'Red', 'Yellow', 'Orange', 'Sky Blue', 'Pink', 'Green', 'Mint', 'Brown', 'Purple', 'Grey', 'Lime Green', 'Black']
+    CHAO_TYPES = ['Monotone', 'Two-tone', 'Jewel Monotone', 'Shiny Monotone', 'Jewel Two-tone', 'Shiny Two-tone', 'Shiny Jewel']
+    GRADE_TO_VALUE = {'F': -1, 'E': 0, 'D': 1, 'C': 2, 'B': 3, 'A': 4, 'S': 5, 'X': 6}
+    STATS = ['Swim', 'Fly', 'Run', 'Power', 'Intel', 'Stamina']
+    GRADES = ['F', 'E', 'D', 'C', 'B', 'A', 'S']
 
     def __init__(self, bot):
         self.bot = bot
 
     def calculate_exp_gain(self, grade: str) -> int:
-        return (self.grade_to_value[grade] * 3) + 13
+        return (self.GRADE_TO_VALUE[grade] * 3) + 13
 
     @commands.command()
     async def give_egg(self, ctx):
-        color = random.choice(self.chao_colors)
-        chao_type = random.choice(self.chao_types)
+        color, chao_type = random.choice(self.CHAO_COLORS), random.choice(self.CHAO_TYPES)
         chao_name = self.bot.cogs['FortuneTeller'].generate_chao_name()
+
         chao_list = await self.bot.cogs['Database'].get_chao(ctx.guild.id, ctx.author.id)
         while any(chao['name'] == chao_name for chao in chao_list):
             chao_name = self.bot.cogs['FortuneTeller'].generate_chao_name()
 
-        grades = ['F', 'E', 'D', 'C', 'B', 'A', 'S']
-        stats = ['Swim', 'Fly', 'Run', 'Power', 'Intel', 'Stamina']
-        chao = {
+        chao_data = {
             'name': chao_name,
             'color': color,
             'type': chao_type,
@@ -38,27 +34,26 @@ class Chao(commands.Cog):
             'birth_date': None,
         }
 
-        for stat in stats:
-            grade = random.choice(grades)
-            chao[f'{stat.lower()}_grade'] = grade
-            chao[f'{stat.lower()}_ticks'] = 0
-            chao[f'{stat.lower()}_exp'] = 0
-            chao[f'{stat.lower()}_level'] = 0
+        for stat in self.STATS:
+            grade = random.choice(self.GRADES)
+            chao_data.update({f'{stat.lower()}_grade': grade, f'{stat.lower()}_ticks': 0, f'{stat.lower()}_exp': 0, f'{stat.lower()}_level': 0})
 
-        await self.bot.cogs['Database'].store_chao(ctx.guild.id, ctx.author.id, chao)
+        await self.bot.cogs['Database'].store_chao(ctx.guild.id, ctx.author.id, chao_data)
         await ctx.send(f"You received a {color} {chao_type} Chao Egg named {chao_name}! It will hatch in 5 seconds.")
         await asyncio.sleep(5)
-        chao['hatched'] = 1
-        chao['birth_date'] = datetime.datetime.utcnow().date()
-        chao['hp_ticks'] = 10
-        await self.bot.cogs['Database'].store_chao(ctx.guild.id, ctx.author.id, chao)
+
+        chao_data['hatched'] = 1
+        chao_data['birth_date'] = datetime.datetime.utcnow().date()
+        chao_data['hp_ticks'] = 10
+
+        await self.bot.cogs['Database'].store_chao(ctx.guild.id, ctx.author.id, chao_data)
         await ctx.send(f"Your {chao_name} Egg has hatched into a {color} {chao_type} Chao named {chao_name}!")
-        log.info(f"Egg given to {ctx.author.id}")
 
     @commands.command()
     async def feed(self, ctx, chao_name: str, *args: str):
         db_cog = self.bot.get_cog('Database')
         item_name = ' '.join(args).rstrip('s').lower()
+
         chao_list = await db_cog.get_chao(ctx.guild.id, ctx.author.id)
         chao_to_feed = next((chao for chao in chao_list if chao['name'].lower() == chao_name.lower()), None)
 
@@ -69,6 +64,7 @@ class Chao(commands.Cog):
         inventory_df = await db_cog.get_inventory(ctx.guild.id, ctx.author.id)
         if inventory_df is not None and not inventory_df[inventory_df['item'].str.lower() == item_name].empty:
             inventory_item = inventory_df.loc[inventory_df['item'].str.lower() == item_name]
+
             if inventory_item.empty or inventory_item.iloc[0]['quantity'] <= 0:
                 await ctx.send(f"You don't have a(n) {item_name} in your inventory.")
                 return
@@ -103,22 +99,20 @@ class Chao(commands.Cog):
             level_up_message = ""
 
             if level_up:
-                stat_level = stat_to_update.rsplit('_', 1)[0] + '_level'
-                stat_exp = stat_to_update.rsplit('_', 1)[0] + '_exp'
-                stat_grade = stat_to_update.rsplit('_', 1)[0] + '_grade'
+                stat_level = f"{stat_to_update.rsplit('_', 1)[0]}_level"
+                stat_exp = f"{stat_to_update.rsplit('_', 1)[0]}_exp"
+                stat_grade = f"{stat_to_update.rsplit('_', 1)[0]}_grade"
                 chao_to_feed[stat_level] += 1
                 exp_gain = self.calculate_exp_gain(chao_to_feed[stat_grade])
                 chao_to_feed[stat_exp] += exp_gain
                 level_up_message = f"\n{chao_name}'s {stat_level.replace('_', ' ')} has increased to level {chao_to_feed[stat_level]} and gained {exp_gain} {stat_exp.replace('_', ' ')}!"
 
-            await self.bot.cogs['Database'].store_chao(ctx.guild.id, ctx.author.id, chao_to_feed)
+            await db_cog.store_chao(ctx.guild.id, ctx.author.id, chao_to_feed)
             await self.bot.cogs['Generator'].generate_image(ctx, chao_name, stat_to_update.rsplit('_', 1)[0], chao_to_feed[stat_to_update])
             await ctx.send(f"You fed a(n) {item_name} to {chao_name}! {chao_name}'s {stat_to_update.replace('_', ' ')} increased by {random_tick_increase}!{level_up_message}")
-            log.info(f"{ctx.author.id} fed {chao_name}")
+
         else:
-            await self.bot.cogs['Database'].store_chao(ctx.guild.id, ctx.author.id, chao_to_feed)
+            await db_cog.store_chao(ctx.guild.id, ctx.author.id, chao_to_feed)
 
 async def setup(bot):
-    cog = Chao(bot)
-    await bot.add_cog(cog)
-    log.info("Chao cog loaded")
+    await bot.add_cog(Chao(bot))
