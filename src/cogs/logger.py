d2626d54610f29266d480a1b8799057bb498c612
nickshouse <nickshouse@proton.me>
2023-11-27 18:43:24 -0600

    Refactor logging setup and event handling
    Consolidated logging setup into a method for easier maintenance and added logging for command invocations. This improves maintainability and enhances visibility into bot activities. No related issues.

--- src/cogs/logger.py
+++ src/cogs/logger.py
@@ -1,23 +1,28 @@
-import logging
+import discord
 from discord.ext import commands
+import logging
+from datetime import datetime
 
 class Logger(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.logger = logging.getLogger(__name__)  # Creating a logger for this cog
+        self.setup_logger()
 
-        self.logger = logging.getLogger('discord')
-        self.logger.setLevel(logging.WARNING)
-        handler = logging.FileHandler(filename='./log.log', encoding='utf-8', mode='w')
-        handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-        self.logger.addHandler(handler)
+    def setup_logger(self):
+        # Set up the logging format and level
+        formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s')
+        file_handler = logging.FileHandler('log.log')
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+        self.logger.setLevel(logging.INFO)
 
     @commands.Cog.listener()
-    async def on_message(self, message):
-        self.logger.info(f"Message from {message.author}: {message.content}")
+    async def on_command(self, ctx):
+        # Log every command invocation
+        self.logger.info(f"Command used: {ctx.command} by {ctx.author} in {ctx.guild}")
 
-    @commands.Cog.listener()
-    async def on_ready(self):
-        self.logger.info('Bot is ready.')
+    # Additional listeners for other events can be added here
 
 async def setup(bot):
     await bot.add_cog(Logger(bot))
